7632ed90386408ccc72746d9956288ebb66fd234
danixuz <danixuz@icloud.com>
2022-04-27 00:05:17 +0300

    Initial Commit

---
+++ DieWithMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DieWithMe
+//
+//  Created by Daniel Spalek on 27/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DieWithMe/DieWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieWithMeApp.swift
+//  DieWithMe
+//
+//  Created by Daniel Spalek on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DieWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
